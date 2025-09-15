149392f5cbb5a289f69a85c39e431b0ccb9850c2
Victor Boehm <victor.b.assumpcao@aluno.senai.br>
2025-09-15 11:25:24 -0300

    feat: adiciona mais select

--- atividades/albuns-select.sql
+++ atividades/albuns-select.sql
@@ -1,3 +1,11 @@
 SELECT * FROM albuns;
 
 SELECT COUNT(*) AS total_albuns FROM albuns;
+
+SELECT nome, musica_mais_ouvida FROM albuns;
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE artistas = 'Travis Scott';
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE id = 3;
