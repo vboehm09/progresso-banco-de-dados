2bee84b05c6a70030f3350eca3d6978d9ccfab4d
Victor Boehm <victor.b.assumpcao@aluno.senai.br>
2025-09-22 14:41:11 -0300

    feat: cria comandos iniciais de update

---
+++ queries/03-update.sql
@@ -0,0 +1,11 @@
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET username = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@email.com';
